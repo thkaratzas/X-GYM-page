3c7f784f6059262f0c1981ceeb63b749643fd0ff
Thanasis <thanasiskaratzas@icloud.com>
2025-04-11 20:18:18 +0300

    Initial Commit

---
+++ X GYM - Attendance tracker/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  X GYM - Attendance tracker
+//
+//  Created by Thanasis Karatzas on 11/4/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
